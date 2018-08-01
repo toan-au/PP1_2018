1c585e5bbc472e2a22972711ae6149ebb0b29ee5
DeftCorgi <toanau123@gmail.com>
2018-08-01 16:13:30 +1000

    added conditional serving of react bundle

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ app.get('/api/cats', (req, res) => {
 
 // if the user's request reaches this point, it means that it is not an api call
 // so they want to actually VIEW our app (react app)
-app.get('*', (req, res) => {
-  const path = require('path');
-  app.use(express.static('./client/build'));
-  // send them the react bundle
-  res.sendFile(path.resolve(__dirname, 'client', 'build'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    const path = require('path');
+    app.use(express.static('./client/build'));
+    // send them the react bundle
+    res.sendFile(path.resolve(__dirname, 'client', 'build'));
+  });
+}
 
 // listen to whatever port heroku serves our app on
 // OR port 5000 (in dev environment)
