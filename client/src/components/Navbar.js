060c18d64cbba46fb158237181528099978fe403
DeftCorgi <toanau123@gmail.com>
2018-08-04 15:18:47 +1000

    extremely basic wire frame styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Build, SupervisorAccount, Home } from 'material-react-icons';
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="Navbar">
-        <a className="logo">
-          <h1>Game Search Match</h1>
-        </a>
-        <Link to="/">Home</Link>
-        <Link to="/">Login</Link>
+        <Link to="/" className="logo">
+          Game Search Match
+        </Link>
+        <div className="right-nav">
+          <Link to="/">
+            <Home />
+          </Link>
+          <Link to="/messages">htr</Link>
+          <Link to="/requests">
+            <SupervisorAccount />
+          </Link>
+          <Link to="/">
+            <Build />
+          </Link>
+        </div>
       </nav>
     );
   }
