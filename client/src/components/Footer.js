5324d8e9b56119d521dfd61d1e6f88b7bacc069e
DeftCorgi <toanau123@gmail.com>
2018-08-06 11:48:30 +1000

    wireframe implemented

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,9 +4,18 @@ import { Link } from 'react-router-dom';
 class Footer extends Component {
   render() {
     return (
-      <footer className="Footer">
-        Footer
-        <Link to="/">copy write terror wrists</Link>
+      <footer className="Footer container">
+        <div className="left-footer">
+          <Link to="/">copy write terror wrists</Link>
+        </div>
+        <div className="right-footer">
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Privacy</Link>
+            <Link to="/">Contact</Link>
+          </nav>
+        </div>
       </footer>
     );
   }
