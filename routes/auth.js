0491e7bcf17e1a5b3f16f817f4fd345cedd0ae87
DeftCorgi <toanau123@gmail.com>
2018-08-10 15:21:56 +1000

    fixed bug

--- routes/auth.js
+++ routes/auth.js
@@ -18,8 +18,18 @@ passport.use(
   )
 );
 
-router.get('/', (req, res) => {
-  console.log('auth route');
+// user authenticates with google
+router.get('/google', (req, res) => {
+  passport.authenticate('google', { scope: ['openid', 'profile', 'email'] });
 });
+
+// callback
+router.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
 
 module.exports = router;
