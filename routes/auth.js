c755839675d75336e2c3faf6c6f8c5fd8448649a
DeftCorgi <toanau123@gmail.com>
2018-08-10 14:18:15 +1000

    added empty google credentials and CB function

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// setup passport to use the google OAuth2 strategy
+passport.use(
+  new googleStrategy(
+    {
+      clientID: '',
+      clientSecret: '',
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {}
+  )
+);
 
 router.get('/', (req, res) => {
-  console.log('auth route')
+  console.log('auth route');
 });
 
 module.exports = router;
