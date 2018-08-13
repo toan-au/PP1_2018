6ccabfe43a54abb26e3959d1b69254531402fea4
Cindy <s3600735@student.rmit.edu.au>
2018-08-13 22:29:42 +1000

    minor changes

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class AboutUs extends Component {
   render() {
     return (
-      <div className="Description">
+      <div className="description">
         <h2>Who Created GameSearchMatch?</h2>
         <p>GameSearchMatch was created by The Terror Wrists for their Programming Project Subject.</p>
         <p>The Terror Wrists consists of: Toan Au, Martin Balakrishnan, Ronald Rinaldy and Cindy Tran.</p>
