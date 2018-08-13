3fccedbee6edabf874de2db2f11b59e603dd6268
Cindy <s3600735@student.rmit.edu.au>
2018-08-13 23:11:24 +1000

    added scss pages and made a few amendments

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -5,34 +5,8 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact">
-        <h3>CONTACT</h3>
-
-          <form id="contactform" target="_blank" method="post" onsubmit="">
-
-            <label>Name
-              <input type="text" name="name" id="name" placeholder="First and Last Name" required>
-            </label>
-
-            <label>Contact Number
-              <input type="tel" name="phone" id="phone" placeholder="Contact Number" required/>
-            </label>
-
-            <label>Email
-              <input type="email" name="email" id="email" placeholder="Email Address" required/>
-            </label>
-
-            <br><br>
-
-            <label>Message
-              <textarea id="contactmessage" name="message" id="message" placeholder="Your Message" required></textarea>
-            </label>
-
-            <br>
-            <br>
-
-            <input class="button" type="submit" value="Submit"/>
-
-          </form>
+        <h3>Contact</h3>
+        
       </div>
     );
   }
