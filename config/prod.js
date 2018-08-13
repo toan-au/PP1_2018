bdb4977e6d28e273276e5b99baffe00c50e85f71
DeftCorgi <toanau123@gmail.com>
2018-08-13 22:05:23 +1000

    added production keys

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,10 @@
 // production keys
 // keys shold be saved in the environment NOT in this file
-const keys = {};
+const keys = {
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  dbString: process.env.DB_STRING,
+  cookieKey: process.env.COOKIE_KEY
+};
 
 module.exports = keys;
