477e986279abf47b5c25852e6bc49a50d4aca23f
DeftCorgi <toanau123@gmail.com>
2018-08-14 00:09:32 +1000

    fixed ev var

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // keys will be kept in this file. dependign on ENV
 // will either export production or dev keys
 
-if (process.env.ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
