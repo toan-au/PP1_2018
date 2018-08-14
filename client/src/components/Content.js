3fc8cdc302dde2eab804be69192d5ac19e9f056c
DeftCorgi <toanau123@gmail.com>
2018-08-14 14:45:44 +1000

    conditionals in place with placeholder components

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -3,13 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 
 // pages
 import Home from '../pages/Home';
+import Landing from '../pages/Landing';
 
 class Content extends Component {
   render() {
     return (
       <main className="Content container">
         <Switch>
-          <Route path="/" component={Home} />
+          {this.props.user && <Route path="/" component={Home} />}
+          <Route path="/" component={Landing} />
         </Switch>
       </main>
     );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,26 +8,50 @@ class Navbar extends Component {
     axios.get('/auth/google');
   }
 
+  renderLoggedIn() {
+    return (
+      <div className="right-nav">
+        <Link to="/">
+          <Home viewBox="10 10 10 10" className="icon" />
+        </Link>
+        <Link to="/messages">
+          <SupervisorAccount viewBox="10 10 10 10" className="icon" />
+        </Link>
+        <Link to="/requests">
+          <SupervisorAccount viewBox="10 10 10 10" className="icon" />
+        </Link>
+        <a href="/auth/google">
+          <Build viewBox="10 10 13 13" className="icon" />
+        </a>
+      </div>
+    );
+  }
+
+  renderLoggedOut() {
+    return (
+      <div className="right-nav">
+        <Link to="/">
+          <Home viewBox="10 10 10 10" className="icon" />
+        </Link>
+      </div>
+    );
+  }
+
   render() {
+    let rightNav;
+    if (this.props.user) {
+      rightNav = this.renderLoggedIn();
+    } else {
+      rightNav = this.renderLoggedOut();
+    }
+
     return (
       <nav className="Navbar">
         <Link to="/" className="logo">
           Game Search Match
         </Link>
-        <div className="right-nav">
-          <Link to="/">
-            <Home viewBox="10 10 10 10" className="icon" />
-          </Link>
-          <Link to="/messages">
-            <SupervisorAccount viewBox="10 10 10 10" className="icon" />
-          </Link>
-          <Link to="/requests">
-            <SupervisorAccount viewBox="10 10 10 10" className="icon" />
-          </Link>
-          <a href="/auth/google">
-            <Build viewBox="10 10 13 13" className="icon" />
-          </a>
-        </div>
+        {/* Conditional rendering of right-nav */}
+        {rightNav}
       </nav>
     );
   }
