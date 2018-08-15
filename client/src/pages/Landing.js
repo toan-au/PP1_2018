6a224ccbb01d684476679b870b59db6a59a0974f
Cindy <s3600735@student.rmit.edu.au>
2018-08-15 22:03:58 +1000

    center aligned div and text both vertically and horizontally

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,11 +4,20 @@ import { Link } from 'react-router-dom';
 class Landing extends Component {
   render() {
     return (
-      <div className="landing">
-        <h2>WELCOME TO GAMESEARCHMATCH</h2>
-        <p>The beautifully designed matching application that will match you with your destined gaming compatriots through our top-tier matching algorithm!</p>
-      </div>
 
+      //<div className="landing">
+        //<h2>WELCOME TO GAMESEARCHMATCH</h2>
+        //<p>The beautifully designed matching application that will match you with your destined gaming compatriots through our top-tier matching algorithm!</p>
+      //</div>
+
+      <div class="outer">
+        <div class="middle">
+          <div class="inner">
+            <h2>WELCOME TO GAMESEARCHMATCH</h2>
+            <p>The beautifully designed matching application that will match you with your destined gaming compatriots through our top-tier matching algorithm!</p>
+          </div>
+        </div>
+      </div>
     )
   }
 }
