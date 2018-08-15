1ad963a1cacfc5c2b9695db9f351792f46a84b72
Cindy <s3600735@student.rmit.edu.au>
2018-08-15 21:53:03 +1000

    landing page changes

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
-    return <div>I am the landing page</div>;
+    return (
+      <div className="landing">
+        <h2>WELCOME TO GAMESEARCHMATCH</h2>
+        <p>The beautifully designed matching application that will match you with your destined gaming compatriots through our top-tier matching algorithm!</p>
+      </div>
+
+    )
   }
 }
 
