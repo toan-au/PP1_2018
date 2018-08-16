a072f3ccb0149237585fcb3ea4e013df8ffa56a8
Cindy <s3600735@student.rmit.edu.au>
2018-08-16 11:26:07 +1000

    added extra text and attempting to add image

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,11 +4,14 @@ import { Link } from 'react-router-dom';
 class Landing extends Component {
   render() {
     return (
-      <div class="outer">
-        <div class="middle">
-          <div class="inner">
+      <div className="outer">
+        <div className="middle">
+          <div className="inner">
             <h2>WELCOME TO GAMESEARCHMATCH</h2>
             <p>The beautifully designed matching application that will match you with your destined gaming compatriots through our top-tier matching algorithm!</p>
+            <br></br>
+            <p>Register or sign in now with a Google Account.</p>
+            <img src="../PP1/client/src/pages/landing.jpg" alt="Group of children playing arcade games"></img>
           </div>
         </div>
       </div>
