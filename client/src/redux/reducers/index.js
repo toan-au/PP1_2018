c044103cca14758d3dc8d684e7749c4dd10d1956
DeftCorgi <toanau123@gmail.com>
2018-08-17 14:23:33 +1000

    added redux-form, added reducer for form

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 
 // reducers
 import user from './user';
 import matches from './matches';
 
 // combine the reducers and export
-export default combineReducers({ user, matches });
+export default combineReducers({ user, matches, form });
