01132c9c50a41e6686ca981e4d3c6d7fa893d3ef
DeftCorgi <toanau123@gmail.com>
2018-08-18 14:34:15 +1000

    fixed bug

--- models/importance.js
+++ models/importance.js
@@ -1,20 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var importance = sequelize.define('importance', {
-    qId: DataTypes.INTEGER,
-    imp1: DataTypes.INTEGER,
-    imp2: DataTypes.INTEGER,
-    imp3: DataTypes.INTEGER,
-    imp4: DataTypes.INTEGER,
-    imp5: DataTypes.INTEGER,
-    imp6: DataTypes.INTEGER,
-    imp7: DataTypes.INTEGER,
-    imp8: DataTypes.INTEGER,
-    imp9: DataTypes.INTEGER,
-    imp10: DataTypes.INTEGER
-  }, {});
+  var importance = sequelize.define(
+    'importance',
+    {
+      qId: DataTypes.INTEGER,
+      imp1: DataTypes.INTEGER,
+      imp2: DataTypes.INTEGER,
+      imp3: DataTypes.INTEGER,
+      imp4: DataTypes.INTEGER,
+      imp5: DataTypes.INTEGER,
+      imp6: DataTypes.INTEGER,
+      imp7: DataTypes.INTEGER,
+      imp8: DataTypes.INTEGER,
+      imp9: DataTypes.INTEGER,
+      imp10: DataTypes.INTEGER
+    },
+    {}
+  );
   importance.associate = function(models) {
-    preferences.belongsTo(models.questionnaires)
+    importance.belongsTo(models.questionnaires);
   };
   return importance;
 };
