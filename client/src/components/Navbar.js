a21b3a319b8d23f1e3157632bd13e4968e9041c0
rmit-s3496839-Ronald-Rinaldy <s3496839@student.rmit.edu.au>
2018-08-19 16:17:52 +1000

    conditional rendering fix
    Nav conditional rendetring fix

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import axios from 'axios';
-
 
 
 class Navbar extends Component {
   googleSignin() {
     axios.get('/auth/google');
   }
+  renderLoggedIn() {
+    return (
+      <div className="right-nav">
+        <a href="/auth/logout" class="button1">
+          Logout
+        </a>
+      </div>
+    );
+  }
+
+  renderLoggedOut() {
+    return (
+      <div className="right-nav">
+        <a href="/auth/google" class="button1">
+          Google login
+        </a>
+      </div>
+    );
+  }
 
   render() 
   {
+    let rightNav;
+    if (this.props.user) {
+      rightNav = this.renderLoggedIn();
+    } else {
+      rightNav = this.renderLoggedOut();
+    }
     return (
       <nav className="Navbar">
         <Link to="/" className="logo">
@@ -26,4 +51,9 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+export default connect(mapStateToProps)(Navbar);
+
