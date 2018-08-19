3783bc98f5b19d2aced7bafd51ee9b7daf0db92a
Cindy <s3600735@student.rmit.edu.au>
2018-08-19 21:24:03 +1000

    content.js changes

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -4,12 +4,15 @@ import { connect } from 'react-redux';
 
 // pages
 import Home from '../pages/Home';
-import Contact from '../pages/Contact';
+import Landing from '../pages/Landing';
 import Registration from '../pages/Registration';
 import FormOne from '../pages/FormOne';
 import FormTwo from '../pages/FormTwo';
 import FormThree from '../pages/FormThree';
 import FormFour from '../pages/FormFour';
+import AboutUs from '../pages/AboutUs';
+import Contact from '../pages/Contact';
+import Privacy from '../pages/Privacy';
 
 class Content extends Component {
   render() {
@@ -20,9 +23,12 @@ class Content extends Component {
           <Route exact path="/FormTwo" component={FormTwo} />
           <Route exact path="/FormThree" component={FormThree} />
           <Route exact path="/FormFour" component={FormFour} />
+          <Route exact path="/AboutUs" component={AboutUs} />
+          <Route exact path="/Privacy" component={Privacy} />
+          <Route exact path="/Contact" component={Contact} />
           {this.props.user && <Route path="/" component={Home} />}
           <Route path="/register" component={Registration} />
-          <Route path="/" component={Contact} />
+          <Route path="/" component={Landing} />
         </Switch>
       </main>
     );
