21db2018de9fbc2278db14727056c8026cdc91fa
DeftCorgi <toanau123@gmail.com>
2018-08-20 12:39:08 +1000

    basic styling and default pfp

--- client/src/components/MatchCard.js
+++ client/src/components/MatchCard.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import defaultPfp from '../images/default_pfp.jpg';
 
 const MatchCard = props => {
-  const { display_name, bio, matchingScore } = props.match;
+  const { pfp, display_name, bio, matchingScore } = props.match;
+  const bioLength = 250;
+  const shortBio = bio.substring(0, bioLength);
   return (
     <div className="MatchCard">
-      <div className="profile-pic" />
+      <div>
+        <img className="profile-pic" src={defaultPfp} />
+      </div>
       <div className="display-name">
         <h3>{display_name}</h3>
       </div>
-      <div className="bio">{bio}</div>
+      <div className="bio">
+        {shortBio}
+        ...
+      </div>
       <div className="button-group">
         <button>Like</button>
         <button>{matchingScore}%</button>
