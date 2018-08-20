c564b4892086b0bb22c41a1c24ca3525093dd3cf
DeftCorgi <toanau123@gmail.com>
2018-08-20 13:30:09 +1000

    heart fills based on percent given as prop

--- client/src/components/MatchMeter.js
+++ client/src/components/MatchMeter.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const MatchMeter = ({ percent }) => {
+  let fill = (percent / 100) * 94;
+  console.log(fill);
+  const styles = {
+    background: 'linear-gradient(red, red) bottom no-repeat',
+    backgroundSize: '100% ' + fill + 'px'
+  };
+
   return (
     <div className="MatchMeter">
-      <div className="heart">{percent}</div>
+      <div className="heart-fill" style={styles}>
+        <div className="heart">{percent}</div>
+      </div>
     </div>
   );
 };
