8005b64ed60004070a5c6867e28a0d73b5df6810
DeftCorgi <toanau123@gmail.com>
2018-08-20 14:07:40 +1000

    successfully masked heart background

--- client/src/components/MatchMeter.js
+++ client/src/components/MatchMeter.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
 const MatchMeter = ({ percent }) => {
-  let fill = (percent / 100) * 94;
-  console.log(fill);
+  const width = 75;
+  const height = 63;
+  let fill = (percent / 100) * height - height * 0.35;
+  fill += 25;
   const styles = {
-    background: 'linear-gradient(red, red) bottom no-repeat',
-    backgroundSize: '100% ' + fill + 'px'
+    heartFill: {
+      height,
+      width,
+      background: 'linear-gradient(red, red) bottom no-repeat',
+      backgroundSize: '100% ' + fill + 'px'
+    },
+    heart: {
+      height,
+      width
+    }
   };
 
   return (
     <div className="MatchMeter">
-      <div className="heart-fill" style={styles}>
-        <div className="heart">{percent}</div>
+      <div className="heart-fill" style={styles.heartFill}>
+        <div className="heart" style={styles.heart} />
       </div>
     </div>
   );
