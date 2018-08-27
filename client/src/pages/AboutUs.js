125a0ea90de649971a16408fe93c4eb1df0c776e
DeftCorgi <toanau123@gmail.com>
2018-08-27 10:06:14 +1000

    cleaned up warnings on the client side

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 class AboutUs extends Component {
   render() {
@@ -8,9 +7,12 @@ class AboutUs extends Component {
         <div className="middle">
           <div className="inner">
             <h3>Who Created GameSearchMatch?</h3>
-            <br/>
-            <p>GameSearchMatch was created by The Terror Wrists for their Programming Project Subject.</p>
-            <br/>
+            <br />
+            <p>
+              GameSearchMatch was created by The Terror Wrists for their
+              Programming Project Subject.
+            </p>
+            <br />
             <p>The Terror Wrists consists of: </p>
             <p>Toan Au, Martin Balakrishnan, Ronald Rinaldy and Cindy Tran.</p>
           </div>
