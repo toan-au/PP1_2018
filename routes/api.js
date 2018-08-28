3c18df3ec9318714060f66e49202b434c7fabe29
MartinBalakrishnan <38675341+MartinBalakrishnan@users.noreply.github.com>
2018-08-29 02:19:37 +1000

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const matching = require('../matchingAlgorithm/match');
+const locale = require('../models').locale;
+const region = require('../models').region;
 
 const router = express.Router();
 
 router.get('/match/:id', async (req, res) => {
   const matches = await matching.findMatches();
   res.send(matches);
+});
+
+router.get('/locales', async (req, res) => {
+  const locales = await locale.findAll();
+  res.send(locales);
+});
+
+router.get('/regions', async (req, res) => {
+  const regions = await region.findAll();
+  res.send(regions);
 });
 
 module.exports = router;
