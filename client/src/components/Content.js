6b20d1339742342637d22bac83f22f4491ca49de
Robert Jeffs <19744067+robertjeffs@users.noreply.github.com>
2018-08-30 14:41:51 +1000

    Implemented auth routes and updated .gitignore
    * Content component now takes children.
    * Created AppSwitch component that handles Public/Protected routes depending on user auth status.
    * Updated .gitignore with .DS_Store.

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,31 +1,10 @@
-import React, { Component } from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-// pages
-import Home from '../pages/Home';
-import Landing from '../pages/Landing';
-import AboutUs from '../pages/AboutUs';
-import Contact from '../pages/Contact';
-import Privacy from '../pages/Privacy';
-import Registration from '../pages/registration/Registration';
-
-class Content extends Component {
-  render() {
-    return (
-      <main className="Content">
-        <Switch>
-          <Route exact path="/register" component={Registration} />
-          <Route exact path="/aboutus" component={AboutUs} />
-          <Route exact path="/privacy" component={Privacy} />
-          <Route exact path="/contact" component={Contact} />
-          {this.props.user && <Route path="/" component={Home} />}
-          <Route path="/" component={Landing} />
-        </Switch>
-      </main>
-    );
-  }
-}
+const Content = ({ children }) => {
+  return <main className="Content">{children}</main>;
+};
 
 const mapStateToProps = state => ({ user: state.user });
 
