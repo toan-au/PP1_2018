87bd3dc9fdcd33d7cf817b85605b849a92399495
Robert Jeffs <19744067+robertjeffs@users.noreply.github.com>
2018-08-30 17:43:35 +1000

    Rerfactored some components to functional components due to being stateless

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="Footer container">
-        <div className="left-footer">
-          <p>© The Terror Wrists 2018</p>
-        </div>
-        <div className="right-footer">
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/aboutus">About Us</Link>
-            <Link to="/privacy">Privacy</Link>
-            <Link to="/contact">Contact</Link>
-          </nav>
-        </div>
-      </footer>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer className="Footer container">
+      <div className="left-footer">
+        <p>© The Terror Wrists 2018</p>
+      </div>
+      <div className="right-footer">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/aboutus">About Us</Link>
+          <Link to="/privacy">Privacy</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
