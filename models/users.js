a6db4afcbbf409a9f5980fd762349166d1c3917e
MartinBalakrishnan <38675341+MartinBalakrishnan@users.noreply.github.com>
2018-09-06 19:52:02 +1000

    Add files via upload

--- models/users.js
+++ models/users.js
@@ -10,12 +10,16 @@ module.exports = (sequelize, DataTypes) => {
     finishedRegistration: DataTypes.BOOLEAN
   }, {});
   users.associate = function(models) {
+    //matching factors
     users.hasMany(models.responses, {foriegnKey: 'userId'})
     users.hasMany(models.prefGames, {foriegnKey: 'userId'})
+    users.hasMany(models.matches, {foriegnKey: 'userId'})
+
+    //user qualities
     users.hasOne(models.locale, {foreignKey:'localeId'})
     users.hasOne(models.region, {foreignKey:'regionId'})
-    users.belongsToMany(models.matches, {through: 'usersMatches'}, {foriegnKey: 'userId'})
-    users.belongsToMany(models.matches, {through: 'usersMatches'}, {foriegnKey: 'matchId'})
+
+    //auth method
     users.belongsTo(models.discordUser, {foriegnKey: 'userId'})
     users.belongsTo(models.googleUser, {foriegnKey: 'userId'})
     users.belongsTo(models.facebookUser, {foriegnKey: 'userId'})
