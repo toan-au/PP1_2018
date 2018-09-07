8d5b3c720b5bdd8947ab9bbe4eebb9232030ff57
MartinBalakrishnan <balakrishnan_martin@live.com>
2018-09-07 18:40:01 +1000

    Updated matches to include non initiated matches, reduced unnecessary data called

--- userFunctions/userCalls.js
+++ userFunctions/userCalls.js
@@ -22,24 +22,27 @@ var getPendingMatches = async function()  {
     var requestId = 1;
 
     //find the requested user, and their matches
-    var currentUser = await users.findOne({where: {id: requestId}, include: [
-        {model: matches}
-    ], plain: true});
+    var findMatches = await matches.findAll({where: {userId: requestId}});
 
 
 
     var pendingUserIds = [];
+
     //extract the id values, from the matches
-    for(var loopCounter = 0; loopCounter < currentUser.matches.length; loopCounter++){
-        var filterArray = currentUser.matches[loopCounter];
-        if(filterArray.matchResponse == 'P')
+    for(var loopCounter = 0; loopCounter < findMatches.length; loopCounter++){
+        var filterArray = findMatches[loopCounter];
+        //If the user Id is equal to the requesting Id, and the user responded Life, while the match has not responded
+        if(filterArray.userResponse == 'L' && filterArray.matchResponse == 'P')
         {
-            pendingUserIds.push(filterArray.id);
+            
+            pendingUserIds.push(filterArray.matchId);
         }
     }
 
+    
+
     //find the pending matches as user objects
-    var pendingUsers = await users.findAll({where: {id:  {[Op.or]: pendingUserIds}}, });
+    var pendingUsers = await users.findAll({where: {id:{[Op.or]: pendingUserIds}}});
 
     //transform the objects to a more reasonable form
     for(var i = 0; i < pendingUsers.length; i++){
@@ -56,20 +59,24 @@ var getSuccessfulMatches = async function(){
     //placeholder Id, will take in variable ID values in future cases
     var requestId = 1;
 
+
     //find the requested user, and their matches
-    var currentUser = await users.findOne({where: {id: requestId}, include: [
-        {model: matches}
-    ], plain: true});
-
-
+    var findMatches = await matches.findAll({where: {[Op.or]:[{userId: requestId},{matchId: requestId}]}});
 
     var matchingUserIds = [];
+    
     //extract the id values, from the matches
-    for(var loopCounter = 0; loopCounter < currentUser.matches.length; loopCounter++){
-        var filterArray = currentUser.matches[loopCounter];
-        if(filterArray.matchResponse == 'L' && filterArray.matchResponse == 'L')
+    for(var loopCounter = 0; loopCounter < findMatches.length; loopCounter++){
+        var filterArray = findMatches[loopCounter];
+        //If the user Id is equal to the requesting Id, and the user responded Like, and the match has liked them back
+        if(filterArray.userId == requestId && filterArray.userResponse == 'L' && filterArray.matchResponse == 'L')
         {
-            matchingUserIds.push(filterArray.id);
+            matchingUserIds.push(filterArray.matchId);
+        }
+        //If the user was not the initiating member of the match, it stores the initating user's id instead
+        if(filterArray.matchId == requestId && filterArray.userResponse == 'L' && filterArray.matchResponse == 'L')
+        {
+            matchingUserIds.push(filterArray.userId);
         }
     }
 
