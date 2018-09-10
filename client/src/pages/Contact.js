c48c4e181cee6a1bdd3b949f4491ab4e6a38476b
DeftCorgi <toanau123@gmail.com>
2018-09-10 21:39:29 +1000

    fixed overlapping styles

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -8,29 +8,44 @@ const Contact = () => {
     <div className="ContactForm">
       <form>
         <h3 className="contact-title">Contact Us</h3>
-        <br/>
+        <br />
         <div className="field">
           <label>Display Name </label>
-          <br/>
-          <input type="text" name="displayName" className="displayName" placeholder="Display Name" required></input>
+          <br />
+          <input
+            type="text"
+            name="displayName"
+            className="displayName"
+            placeholder="Display Name"
+            required
+          />
         </div>
 
         <div className="field">
           <label>Email </label>
-          <br/>
-          <input type="email" name="email" className="email" placeholder="Email Address" required></input>
+          <br />
+          <input
+            type="email"
+            name="email"
+            className="email"
+            placeholder="Email Address"
+            required
+          />
         </div>
 
         <div className="field">
           <label>Your message </label>
-          <br/>
-          <textarea name="message" className="message" placeholder="Type here..." /*required*/></textarea>
+          <br />
+          <textarea
+            name="message"
+            className="message"
+            placeholder="Type here..." /*required*/
+          />
         </div>
 
         <button className="submit" type="submit">
           Submit
         </button>
-
       </form>
     </div> //contact
   );
