7b00f306b454ec07059d542db0dd3eb6a97ecdad
Robert Jeffs <19744067+robertjeffs@users.noreply.github.com>
2018-09-11 19:23:31 +1000

    Increase consistancy of pending state

--- client/src/redux/actions/pending.js
+++ client/src/redux/actions/pending.js
@@ -4,9 +4,12 @@ import axios from 'axios';
 export const getPending = id => {
   return async dispatch => {
     const response = await axios.get('/api/matches/pending/' + id);
+
+    // response.data is an array of users. If empty, set matches to null.
+    const pendingMatches = response.data.length > 0 ? response.data : null;
     const pending = {
       loading: false,
-      matches: response.data
+      matches: pendingMatches
     };
     dispatch({ type: GET_PENDING, pending: pending });
   };
