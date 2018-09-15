f1a1c616be602a995b3c4f9054a9e49297df570c
DeftCorgi <toanau123@gmail.com>
2018-09-15 19:16:00 +1000

    better ratings implemented

--- client/src/pages/Matches.js
+++ client/src/pages/Matches.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getMatched } from '../redux/actions/matched';
 import defaultPfp from '../images/fortnite_drift_.png';
-import StarRating from 'react-star-rating';
+import ReactStars from 'react-stars';
 
 class Matches extends Component {
+  state = {
+    ratings: {}
+  };
+
   componentDidMount = async () => {
     await this.props.getMatched(this.props.user.id);
-    console.log(this.props.matched);
+  };
+
+  rate = (matchId, rating) => {
+    // redux action to like user
+    const ratings = this.state.ratings;
+    ratings[matchId] = rating;
+    this.setState({ ratings });
+    console.log(matchId, rating);
   };
 
   renderMatched = () => {
@@ -28,27 +39,13 @@ class Matches extends Component {
         </a>
         <div className="rate-user">
           Rate {match.displayName}:<br />
-          <div className="rate">
-            <input type="radio" id="star5" name="rate" value="5" />
-            <label for="star5" title="text">
-              5 stars
-            </label>
-            <input type="radio" id="star4" name="rate" value="4" />
-            <label for="star4" title="text">
-              4 stars
-            </label>
-            <input type="radio" id="star3" name="rate" value="3" />
-            <label for="star3" title="text">
-              3 stars
-            </label>
-            <input type="radio" id="star2" name="rate" value="2" />
-            <label for="star2" title="text">
-              2 stars
-            </label>
-            <input type="radio" id="star1" name="rate" value="1" />
-            <label for="star1" title="text">
-              1 star
-            </label>
+          <div>
+            <ReactStars
+              count={5}
+              value={this.state.ratings[match.id]}
+              onChange={rating => this.rate(match.id, rating)}
+              size={40}
+            />
           </div>
         </div>
       </li>
