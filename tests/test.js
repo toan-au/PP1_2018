348e16091202a38d48a214b77d251df143daef83
DeftCorgi <toanau123@gmail.com>
2018-09-20 13:00:34 +1000

    installed mocha/chai

---
+++ tests/test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('Key files', () => {
+  describe('prod.js', () => {
+    it('Should exist', () => {
+      assert.equal(1, 1);
+    });
+  });
+});
