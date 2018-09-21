9a014a54a5e5563eda0d4e67e11ec04562d20368
DeftCorgi <toanau123@gmail.com>
2018-09-21 16:55:25 +1000

    dummy deleted, supressed logs

--- client/src/redux/reducers/notifications.js
+++ client/src/redux/reducers/notifications.js
@@ -1,15 +1,6 @@
 import { ADD_NOTIFICATION, REMOVE_NOTIFICATION } from '../actions/types';
 
-const defaultNotes = [
-  { id: 0, text: 'Stuff done', color: 'red' },
-  { id: 1, text: 'Friend added', color: 'red' },
-  { id: 2, text: 'Match disliked', color: 'red' },
-  { id: 3, text: 'Match liked', color: 'red' },
-  { id: 4, text: 'User rated', color: 'blue' }
-];
-
 export default (state = defaultNotes, action) => {
-  console.log(state);
   switch (action.type) {
     case ADD_NOTIFICATION:
       return [...state, action.note];
