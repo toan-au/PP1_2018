51d9edf61a87d5cebd023bd6db5a7e23d836a713
DeftCorgi <toanau123@gmail.com>
2018-09-21 15:36:56 +1000

    initial notification stack copmonent

---
+++ client/src/components/Notifications.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import connect from 'react-redux';
+
+const notifications = [{ id: 0, component: <div>hello world</div> }];
+
+const Notifications = () => {
+  return (
+    <div className="Notifications">
+      <ul>
+        {notifications.map(note => (
+          <div>{note.id}</div>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const mapStateToProps = (state = {});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(Notifications);
