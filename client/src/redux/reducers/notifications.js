45d5c20a822cc36baa2fba9ed6cb554d68c7e972
DeftCorgi <toanau123@gmail.com>
2018-09-21 15:58:05 +1000

    dummy data added

--- client/src/redux/reducers/notifications.js
+++ client/src/redux/reducers/notifications.js
@@ -1,6 +1,14 @@
 import { ADD_NOTIFICATION, REMOVE_NOTIFICATION } from '../actions/types';
 
-export default (state = [], action) => {
+const defaultNotes = [
+  { id: 0, text: 'Stuff done', color: 'red' },
+  { id: 1, text: 'Friend added', color: 'red' },
+  { id: 2, text: 'Match disliked', color: 'red' },
+  { id: 3, text: 'Match liked', color: 'red' },
+  { id: 4, text: 'User rated', color: 'blue' }
+];
+
+export default (state = defaultNotes, action) => {
   switch (action.type) {
     case ADD_NOTIFICATION:
       return [...state, action.note];
