2ca93f3468107b340506e244e14407ad3908f41e
DeftCorgi <toanau123@gmail.com>
2018-09-21 14:43:01 +1000

    now can filter on 2 properties

---
+++ config/test.js
@@ -0,0 +1,18 @@
+// development keys
+// do NOT commit this file to github, secret information
+const keys = {
+  googleClientId:
+    '224760672447-sk3mf5kkgjfuh75auo6v4ft0j2kv2ug5.apps.googleusercontent.com',
+  googleClientSecret: 'SZRPWRJDpXU4P2CP1p8V_FVy',
+  googleProjectId: 'game-search-match-215703',
+  gcsBucketName: 'pfps',
+  facebookClientId: '2387481741478860',
+  facebookClientSecret: 'f407e7743f4ea9580b4cd8dd619a6315',
+  discordClientId: '487818315696308234',
+  discordClientSecret: 'BNkbdZPBBLDBbxEz4FDxKBSfliPSI1_1',
+  dbString:
+    'postgres://pvmswwfi:uqGWWRdHDeuWeGVcb3V5ah_vVjaK8GC0@elmer.db.elephantsql.com:5432/pvmswwfi',
+  cookieKey: 'holaamiga'
+};
+
+module.exports = keys;
